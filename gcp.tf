c9d70b41b22cab31efda1f69f21fca30c2ed44ee
Brad Sickles <brad.sickles@gmail.com>
2026-06-25 10:14:08 -0400

    enable apis

--- gcp.tf
+++ gcp.tf
@@ -3,3 +3,10 @@ data "google_project" "this" {}
 locals {
   project_id = data.google_project.this.project_id
 }
+
+# Gemini Data Analytics / Conversational Analytics
+resource "google_project_service" "geminidataanalytics" {
+  service                    = "geminidataanalytics.googleapis.com"
+  disable_dependent_services = false
+  disable_on_destroy         = false
+}
